7b24a1bbdb2ed9a0bdc66e759c94e26679fc2817
coisas <68257024+coisasgamer4@users.noreply.github.com>
2024-04-25 10:17:29 -0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -10,24 +10,23 @@ const About = () => {
         <div className="about-container">
           <div className="about-card">
             <div>
-              <h4 className="h-color">⚡ Commands</h4>
+              <h4 className="h-color">Commands</h4>
               <p className="p-color">
-                We have more than 200 commands support in this bot.
+                This bot is in development and will have more than 100 commands
               </p>
               <hr />
             </div>
             <div>
-              <h4 className="h-color">✉️ 24/7 Support</h4>
+              <h4 className="h-color">Support</h4>
               <p className="p-color">
-                The biggest support community server for the bot available for
-                you anytime.
+              Join our discord to have support at any time
               </p>
               <hr />
             </div>
             <div>
-              <h4 className="h-color">✏️ Customization</h4>
+              <h4 className="h-color">Customization</h4>
               <p className="p-color">
-                Fully customizable discord bot completely FREE!
+                Free discord bot with no premium functionalities, all commands are free to use
               </p>
               <hr />
             </div>
@@ -41,15 +40,15 @@ const About = () => {
 
         <section className="about-stats">
           <div className="about-stats-card">
-            <h4 className="h-color">17k+</h4>
+            <h4 className="h-color">7</h4>
             <p className="p-color">Total Servers</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">94k+</h4>
+            <h4 className="h-color">59</h4>
             <p className="p-color">Total Users</p>
           </div>
           <div className="about-stats-card">
-            <h4 className="h-color">375k+</h4>
+            <h4 className="h-color">135</h4>
             <p className="p-color">Total Commands Ran</p>
           </div>
         </section>
